d86b4c41a41a3d1d4285e29b1997e1da32c43c2d
M.haidar alkahf <mmbcsemesta@gmail.com>
2026-05-21 13:33:14 +0700

    add global CORS headers

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ ffmpeg.setFfmpegPath(ffmpegStatic);
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    if (req.method === "OPTIONS") return res.sendStatus(200);
+    next();
+});
 app.use(express.static("public"));
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
